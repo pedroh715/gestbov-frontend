0e08a6fb35bd358cab39b7356b113661fee1df6e
Pedro Grandizoli <pedrohenriquecontato04@gmail.com>
2022-02-17 16:45:26 -0400

    implementing token authentication

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
+
+import { getUsers } from '../../services/api'
+
 
 const HomePage = () => {
-    return <h1>homePage</h1>
+    const { logout } = useContext(AuthContext)
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        (async () => {
+            const response = await getUsers()
+            setUsers(response.data)
+            setLoading(false)
+        })()
+    }, [])
+    const handleLogout = () => {
+        logout()
+    }
+
+    if(loading) {
+        return <div className="loading">Carregando dados...</div>
+    }
+
+    return(
+        <>
+            <h1>HomePage</h1>
+            <button onClick={handleLogout}>Logout</button>
+            <ul>
+                {
+                    users.map((user) => (
+                        <li key={user._id}>
+                            {user._id} - {user.email}
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
 }
 
 export default HomePage
